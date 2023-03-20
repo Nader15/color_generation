eee73b35e1a78eab551c8ca33ed2ab9689a07aeb
Nader <nadersalah20201111@gmail.com>
2023-03-20 06:11:44 +0200

    floating Button added to make color animation

--- lib/Screens/Home_Screen/home_page.dart
+++ lib/Screens/Home_Screen/home_page.dart
@@ -11,12 +11,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool animation = false;
 
   @override
   void initState() {
     super.initState();
   }
 
+  List<Color> _colors = [Colors.white];
+  Timer? _timer;
+  makeColorAnimation(){
+    setState(() {
+      animation = !animation;
+    });
+    if(animation){
+      _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+        final random = math.Random();
+        final newColor = Color.fromRGBO(
+          random.nextInt(256),
+          random.nextInt(256),
+          random.nextInt(256),
+          1,
+        );
+        setState(() {
+          _colors.add(newColor);
+        });
+      });
+    }else{
+      _timer?.cancel();
+    }
+  }
 
   Color _backgroundColor = Colors.white;
   void _setRandomColor() {
@@ -32,6 +56,11 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,9 +69,15 @@ class _MyHomePageState extends State<MyHomePage> {
         _setRandomColor();
       },
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            makeColorAnimation();
+          },
+          child: Icon(animation?Icons.pause:Icons.play_arrow),
+        ),
         body: AnimatedContainer(
           duration: const Duration(milliseconds: 500),
-          color: _backgroundColor,
+          color: animation?_colors.last:_backgroundColor,
           curve: Curves.easeInOut,
           child: Center(
             child: Text("Hello there",style: textStyle,),
